f2239c65816d7232e499cd8befcaa344a4b9afe9
Ayat <alkayedayat93@gmail.com>
2022-05-29 21:15:52 +0300

    docker

--- movie_library/movies/api/veiwstes.py
+++ movie_library/movies/api/veiwstes.py
@@ -3,8 +3,8 @@ from rest_framework.generics import ( ListCreateAPIView,
                                                         )
 from movies.models import Movie
 from .serializers import Movieserializers
-from .permissions import IsAuthorOrReadOnly
-
+from .permissions import IsAuthorOrReadOnly 
+from rest_framework.permissions import IsAuthenticated
 
 class MovieListCreateAPIView(ListCreateAPIView):
     queryset = Movie.objects.all()
@@ -13,4 +13,4 @@ class MovieListCreateAPIView(ListCreateAPIView):
 class MovieRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = Movieserializers
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly, IsAuthenticated )
