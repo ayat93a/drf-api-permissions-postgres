fb3537250ed3c9458acb89ab84c1891e1bb58372
Ayat <alkayedayat93@gmail.com>
2022-05-30 10:01:25 +0300

    postgresql

--- movie_library/movies_library/settings.py
+++ movie_library/movies_library/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from django import db
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,9 +80,18 @@ WSGI_APPLICATION = 'movies_library.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST' : 'db', 
+        'PORT' : 5432
+
     }
 }
 
