17b5d4f7ceae9439c36a225a86abd7ea771a0e51
Ayat <alkayedayat93@gmail.com>
2022-05-30 21:29:21 +0300

    add auth

--- movie_library/movies/urls.py
+++ movie_library/movies/urls.py
@@ -1,8 +1,18 @@
 from re import M
 from django.urls import path
 from movies.api.veiwstes import MovieListCreateAPIView , MovieRetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt import views as jwt_view
 
 urlpatterns  = [
     path('movies-list' , MovieListCreateAPIView.as_view() , name= 'list'),
     path('<int:pk>' ,MovieRetrieveUpdateDestroyAPIView.as_view() , name= 'detail'),
+    path('token' , jwt_view.TokenObtainPairView.as_view() , name = 'token_obtain'),
+    path('refresh' , jwt_view.TokenRefreshView.as_view() , name = 'refresh'),
 ]
+
+
+
+# {
+#     "refresh": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY1NDAyMTYwOCwiaWF0IjoxNjUzOTM1MjA4LCJqdGkiOiJiZTM0MWYyZWFiNmU0NzA2OWZjZDFmNzU5MzRmNTFkNCIsInVzZXJfaWQiOjF9.5DSuPQ2NLVZWyCYtoi83XnT6TY6kV2NqXw6TGWTyFMc",
+#     "access": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUzOTM1NTA4LCJpYXQiOjE2NTM5MzUyMDgsImp0aSI6ImNiZWM2YzFiMzBlYTQ5YTViOTMyOWQ3Y2MxMGIzNjY5IiwidXNlcl9pZCI6MX0.r6kR3PCv2ZWuzdQ_OyB5MYjYdDi0BJXSz60fbFXZwkY"
+# }
--- movie_library/movies_library/settings.py
+++ movie_library/movies_library/settings.py
@@ -137,7 +137,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES":[
-        "rest_framework.permissions.IsAuthenticated",
-    ]
+        'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+],
+        'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
